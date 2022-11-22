b4ac3bc4c40fd22410fd379ae6ccde3d35cd93b9
unknown <git.markan2nypascual@gmail.com>
2022-11-22 14:28:43 +0800

    Updated Project card background color to gradient

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -10,11 +10,11 @@ export const Project = () => {
       <div>
         <SectionTitle title={`Projects`} />
         {/* Grid */}
-        <div className="grid md:grid-cols-2 gap-4 xl:grid-cols-3 xl:gap-x-8">
+        <div className="grid md:grid-cols-2 gap-4 xl:grid-cols-3 xl:gap-x-4">
           {/* Card */}
-          <div className="rounded-md mb-5 shadow-lg overflow-hidden w-75% bg-gradient-to-br from-cyan-200 to-pink-200">
+          <div className="rounded-md mb-5 shadow-lg overflow-hidden block md:w-[85%] md:mx-auto bg-gradient-to-br from-cyan-200 to-pink-200">
             {/* Image wrapper */}
-            <div className="w-full p-1 md:p-8">
+            <div className="w-full p-1 xl:p-6">
               <div>
                 <img
                   src={project1}
@@ -23,8 +23,78 @@ export const Project = () => {
                 />
               </div>
             </div>
-            <div className="px-3 md:px-8 py-6">
-              <h2 className="font-extrabold text-xl tracking-wide font-squada text-slate-500">
+            <div className="px-3 xl:px-6 py-3">
+              <h2 className="font-extrabold text-xl md:text-2xl tracking-wide font-squada text-slate-500">
+                Avenfashion Manufacturing Inc.
+              </h2>
+              <div className="flex justify-between items-center my-2">
+                <div className="flex gap-x-3">
+                  <span className="text-orange-400">REACT</span>
+                  <span className="text-blue-400">TAILWIND</span>
+                </div>
+                <div className="flex items-center  gap-x-3 text-lg">
+                  <a href="#">
+                    <Fa.FaLink className="text-slate-500" />
+                  </a>
+                  <a href="#">
+                    <Fa.FaGithub className="text-slate-500" />
+                  </a>
+                </div>
+              </div>
+              <p className="py-3 text-md text-slate-500">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                recusandae a doloremque atque, aperiam doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="rounded-md mb-5 shadow-lg overflow-hidden block md:w-[85%] md:mx-auto bg-gradient-to-br from-cyan-200 to-pink-200">
+            {/* Image wrapper */}
+            <div className="w-full p-1 xl:p-6">
+              <div>
+                <img
+                  src={project1}
+                  alt=""
+                  className="hover:scale-105 transition"
+                />
+              </div>
+            </div>
+            <div className="px-3 xl:px-6 py-3">
+              <h2 className="font-extrabold text-xl md:text-2xl tracking-wide font-squada text-slate-500">
+                Avenfashion Manufacturing Inc.
+              </h2>
+              <div className="flex justify-between items-center my-2">
+                <div className="flex gap-x-3">
+                  <span className="text-orange-400">REACT</span>
+                  <span className="text-blue-400">TAILWIND</span>
+                </div>
+                <div className="flex items-center  gap-x-1 text-lg">
+                  <a href="#">
+                    <Fa.FaLink className="text-slate-500" />
+                  </a>
+                  <a href="#">
+                    <Fa.FaGithub className="text-slate-500" />
+                  </a>
+                </div>
+              </div>
+              <p className="py-3 text-md text-slate-500">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                recusandae a doloremque atque, aperiam doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="rounded-md mb-5 shadow-lg overflow-hidden block md:w-[85%] md:mx-auto bg-gradient-to-br from-cyan-200 to-pink-200">
+            {/* Image wrapper */}
+            <div className="w-full p-1 xl:p-6">
+              <div>
+                <img
+                  src={project1}
+                  alt=""
+                  className="hover:scale-105 transition"
+                />
+              </div>
+            </div>
+            <div className="px-3 xl:px-6 py-3">
+              <h2 className="font-extrabold text-xl md:text-2xl tracking-wide font-squada text-slate-500">
                 Avenfashion Manufacturing Inc.
               </h2>
               <div className="flex justify-between items-center my-2">
